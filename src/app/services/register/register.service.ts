69a355984ec8f269dd435f37f4f2a9a93aaca4cc
perdi8 <perdi666@gmail.com>
2021-05-10 13:14:49 +0200

    upload register service

---
+++ src/app/services/register/register.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  constructor(private http: HttpClient) {}
+
+  register(user: any): Observable<any> {
+    let body = {
+      email: user.email,
+      name: user.username,
+      password: user.password,
+    };
+
+    return this.http.post(
+      'https://final-project-ingenia.herokuapp.com/api/register',
+      body
+    );
+  }
+}
