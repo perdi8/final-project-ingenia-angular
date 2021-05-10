0cda49fefb7439329c8f25539bcff25aa9410caa
perdi8 <perdi666@gmail.com>
2021-05-10 22:43:09 +0200

    a

--- src/app/Pages/register-page/register-page.component.ts
+++ src/app/Pages/register-page/register-page.component.ts
@@ -5,9 +5,9 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { DialogDeleteTagComponent } from 'src/app/Components/dialog-delete-tag/dialog-delete-tag.component';
 import { RegisterService } from 'src/app/services/register/register.service';
 
 @Component({
@@ -17,11 +17,13 @@ import { RegisterService } from 'src/app/services/register/register.service';
 })
 export class RegisterPageComponent implements OnInit {
   textValue: string = '';
+  name = 'Email existente';
 
   constructor(
     private formBuilder: FormBuilder,
     private registerService: RegisterService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -44,10 +46,34 @@ export class RegisterPageComponent implements OnInit {
         this.router.navigate(['/login']);
       },
       (err) => {
+        const dialogConfig = new MatDialogConfig();
+
+        dialogConfig.disableClose = false;
+        dialogConfig.autoFocus = true;
+
+        const dialogRef = this.dialog.open(
+          DialogDeleteTagComponent,
+          dialogConfig
+        );
+
+        dialogRef.afterClosed().subscribe((data) => {});
+
         500;
         console.log(err);
         console.log('email existente');
       }
     );
   }
+}
+function DialogOverviewExampleDialog(
+  DialogOverviewExampleDialog: any,
+  arg1: { width: string; data: { name: any; animal: any } }
+) {
+  throw new Error('Function not implemented.');
+}
+function PopupCourseComponent(
+  PopupCourseComponent: any,
+  dialogConfig: MatDialogConfig<any>
+) {
+  throw new Error('Function not implemented.');
 }
