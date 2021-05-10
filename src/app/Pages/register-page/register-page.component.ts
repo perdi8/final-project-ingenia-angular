5cbb4ef4eb0b5a7a68b6414ac60653b96373ac9f
perdi8 <perdi666@gmail.com>
2021-05-10 12:53:53 +0200

    register page

--- src/app/Pages/register-page/register-page.component.ts
+++ src/app/Pages/register-page/register-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent implements OnInit {
+  textValue: string = '';
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    });
   }
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+  });
 
+  register() {
+    console.log(this.registerForm);
+  }
 }
